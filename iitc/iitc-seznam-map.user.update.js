f961169e7955cb766a8967306403dd9d12982655
MarekKnapek <knapek.mar@gmail.com>
2015-09-03 00:21:24 +0200

    incernase to v0.2

---
+++ iitc/iitc-seznam-map.user.update.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @id           iitc-plugin-seznam
+// @name         IITC map layer from seznam.cz
+// @category     Map Tiles
+// @namespace    iitcseznamcz
+// @description  IITC map layer from seznam.cz
+// @author       Marek
+// @version      0.2
+// @downloadURL  https://github.com/MarekKnapek/stuff/raw/release/iitc/iitc-seznam-map.user.js
+// @updateURL    https://github.com/MarekKnapek/stuff/raw/release/iitc/iitc-seznam-map.user.update.js
+// @include      https://www.ingress.com/intel*
+// @include      http://www.ingress.com/intel*
+// @match        https://www.ingress.com/intel*
+// @match        http://www.ingress.com/intel*
+// @include      https://www.ingress.com/mission/*
+// @include      http://www.ingress.com/mission/*
+// @match        https://www.ingress.com/mission/*
+// @match        http://www.ingress.com/mission/*
+// @grant        none
+// ==/UserScript==
+
