3df33f2641a894014e7d2a43e20a5818f3479eaa
MarekKnapek <knapek.mar@gmail.com>
2015-09-03 00:16:53 +0200

    add update info

--- iitc/iitc-seznam-map.user.js
+++ iitc/iitc-seznam-map.user.js
@@ -6,6 +6,8 @@
 // @description  IITC map layer from seznam.cz
 // @author       Marek
 // @version      0.1
+// @downloadURL  https://github.com/MarekKnapek/stuff/raw/release/iitc/iitc-seznam-map.user.js
+// @updateURL    https://github.com/MarekKnapek/stuff/raw/release/iitc/iitc-seznam-map.user.update.js
 // @include      https://www.ingress.com/intel*
 // @include      http://www.ingress.com/intel*
 // @match        https://www.ingress.com/intel*
