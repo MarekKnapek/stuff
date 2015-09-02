665ec299ae8a615d03c5793c5d83d32b9f10245a
MarekKnapek <knapek.mar@gmail.com>
2015-09-02 21:10:20 +0200

    works on more sites (like portal bookmark) now

--- iitc-seznam-map.user.js
+++ iitc-seznam-map.user.js
@@ -6,7 +6,14 @@
 // @version      0.1
 // @description  IITC map layer from seznam.cz
 // @author       Marek
-// @match        https://www.ingress.com/intel
+// @include      https://www.ingress.com/intel*
+// @include      http://www.ingress.com/intel*
+// @match        https://www.ingress.com/intel*
+// @match        http://www.ingress.com/intel*
+// @include      https://www.ingress.com/mission/*
+// @include      http://www.ingress.com/mission/*
+// @match        https://www.ingress.com/mission/*
+// @match        http://www.ingress.com/mission/*
 // @grant        none
 // ==/UserScript==
 
