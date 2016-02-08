1159c61ffaaf8b6612d96d1ac435a9c6a36cd6cc
Marek Knápek <knapek.mar@gmail.com>
2016-02-08 23:02:44 +0100

    Version 0.2.

--- iitc/osma.meta.js
+++ iitc/osma.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       	osma
 // @namespace  	https://github.com/MarekKnapek
-// @version    	0.1
+// @version    	0.2
 // @description	osma
 // @author     	Marek Knápek
 // @updateURL  	https://raw.githubusercontent.com/MarekKnapek/stuff/master/iitc/osma.meta.js
